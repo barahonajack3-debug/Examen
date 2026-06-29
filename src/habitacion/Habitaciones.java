a566b0533e42cb3cfd47d9dbc859ec5fa3ec5e51
Jaxander <barahonajack3@gmail.com>
2026-06-29 15:01:47 -0600

    Se arreglo la clase habitacion
    Se modifico los metodos set, get, constructor y toString

--- src/habitacion/Habitaciones.java
+++ src/habitacion/Habitaciones.java
@@ -4,10 +4,39 @@
  */
 package habitacion;
 
+import alojamiento.Alojamientos;
+
 /**
  *
  * @author Student
  */
-public class Habitaciones {
+public class Habitaciones extends Alojamientos{
+    //Atributos
+    private int NumeroPiso;
+    private boolean Balcon;
+    
+    //Metodos get
+    public int getNumeroHabitacion() {
+        return NumeroPiso;
+    }
+    public boolean isBalcon() {
+        return Balcon;
+    }
+    
+    //Metodos set
+    public void setNumeroHabitacion(int NumeroPiso) {
+        this.NumeroPiso = NumeroPiso;
+    }
+    public void setBalcon(boolean Balcon) {
+        this.Balcon = Balcon;
+    }
+    
+    //Constructor
+    public Habitaciones(int NumeroPiso, boolean Balcon, int Codido, String Nombre, int CapacidadMaxima, int PrecioNoche, boolean Estado) {
+        super(Codido, Nombre, CapacidadMaxima, PrecioNoche, Estado);
+        this.NumeroPiso = NumeroPiso;
+        this.Balcon = Balcon;
+    }
+   
     
 }
